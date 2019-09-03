cb5a5f9bfb434b7ec926038fa8cbe293cbefbb00
abensaad-efrontech <abensaad@efrontech.com>
2019-09-03 11:54:06 +0200

    y

--- target/tomcat.8080/webapps/expanded/resources/js/index.js
+++
@@ -1,7 +0,0 @@
-// copy
-balapaCop("3D Hover Interaction", "rgba(255,255,255,.5)");
-$('#hovertableau tr').hover(function() {
-	$(this).addClass('hover');
-}, function() {
-	$(this).removeClass('hover');
-});
